094793672f11c6918b4e99a13f731d97b1e47e04
Rate980 <xv0nnj0@icloud.com>
2023-02-08 08:46:36 +0900

    Fix 循環参照

--- checks.py
+++ checks.py
@@ -4,34 +4,39 @@ import discord
 from discord.ext.commands import Context, check
 
 from jsons import Jsons
-from play_sound import PlaySound
+
+# from play_sound import PlaySound
 
 
-@check
-async def is_dj(ctx: Context):
-    if ctx.cog is None:
-        return False
-    if not isinstance(ctx.cog, PlaySound):
-        return False
+def is_dj():
+    async def inner(ctx: Context):
+        if ctx.cog is None:
+            return False
+        if not hasattr(ctx.cog, "data") or not isinstance(ctx.cog.data, Jsons):  # type: ignore
+            return False
 
-    data = cast(Jsons, ctx.cog.data)
-    if ctx.guild is None:
-        return False
-    option = data.get_option(ctx.guild.id)
-    if (dj_id := option["dj_id"]) is None:
-        return True
-    if not isinstance(ctx.author, discord.Member):
-        return False
-
-    for x in ctx.author.roles:
-        if x.id == dj_id:
+        data = cast(Jsons, ctx.cog.data)  # type: ignore
+        if ctx.guild is None:
+            return False
+        option = data.get_option(ctx.guild.id)
+        if (dj_id := option["dj_id"]) is None:
             return True
-    else:
-        return False
+        if not isinstance(ctx.author, discord.Member):
+            return False
+
+        for x in ctx.author.roles:
+            if x.id == dj_id:
+                return True
+        else:
+            return False
+
+    return check(inner)
 
 
-@check
-def is_mod(ctx: Context):
-    if not isinstance(ctx.author, discord.Member):
-        return False
-    return ctx.author.guild_permissions.manage_roles
+def is_mod():
+    async def inner(ctx: Context):
+        if not isinstance(ctx.author, discord.Member):
+            return False
+        return ctx.author.guild_permissions.manage_roles
+
+    return check(inner)
